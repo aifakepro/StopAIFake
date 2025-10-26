a045f019065ed3faf66ca578de2b01b9b01c1e18
aifakepro <aifake.pro@gmail.com>
2025-10-26 23:21:21 +0200

    Update BasicFace.tsx

--- components/demo/basic-face/BasicFace.tsx
+++ components/demo/basic-face/BasicFace.tsx
@@ -59,7 +59,7 @@ export default function BasicFace({
   // Load texture and hat images
   useEffect(() => {
     const TEXTURE_URL = texturePath || 'https://images.unsplash.com/photo-1557672172-298e090bd0f1?w=500';
-    const HAT_URL = hatPath || 'https://cdn-icons-png.flaticon.com/512/2919/2919906.png';
+    const HAT_URL = hatPath || 'https://i.ibb.co/TDnPTYzR/gptacp.jpg';
     
     if (TEXTURE_URL) {
       const texture = new Image();

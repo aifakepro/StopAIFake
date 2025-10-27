78cdf159a49dd2497ebfc65fed56cd5fc372864c
aifakepro <aifake.pro@gmail.com>
2025-10-27 15:40:28 +0200

    Update BasicFace.tsx

--- components/demo/basic-face/BasicFace.tsx
+++ components/demo/basic-face/BasicFace.tsx
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
 */
 import { RefObject, useEffect, useState, useRef } from 'react';
+
 import { renderBasicFace } from './basic-face-render';
+
 import useFace from '../../../hooks/demo/use-face';
 import useHover from '../../../hooks/demo/use-hover';
 import useTilt from '../../../hooks/demo/use-tilt';
@@ -11,6 +13,7 @@ import { useLiveAPIContext } from '../../../contexts/LiveAPIContext';
 
 // Minimum volume level that indicates audio output is occurring
 const AUDIO_OUTPUT_DETECTION_THRESHOLD = 0.05;
+
 // Amount of delay between end of audio output and setting talking state to false
 const TALKING_STATE_COOLDOWN_MS = 2000;
 
@@ -21,30 +24,23 @@ type BasicFaceProps = {
   radius?: number;
   /** The color of the face. */
   color?: string;
-  /** URL текстуры для круга (можно использовать прямую ссылку на изображение) */
-  textureUrl?: string;
-  /** URL изображения шапки (можно использовать прямую ссылку на изображение) */
-  hatUrl?: string;
 };
 
 export default function BasicFace({
   canvasRef,
   radius = 250,
   color,
-  textureUrl = "https://i.ibb.co/gFMKwxmR/BACKLogo3.png", // СЮДА вставьте ссылку на текстуру
-  hatUrl = "https://i.ibb.co/d4tfjJ1K/kapBot.png", // СЮДА вставьте ссылку на шапку
 }: BasicFaceProps) {
   const timeoutRef = useRef<NodeJS.Timeout>(null);
-  const textureImageRef = useRef<HTMLImageElement | null>(null);
-  const hatImageRef = useRef<HTMLImageElement | null>(null);
-  
+
   // Audio output volume
   const { volume } = useLiveAPIContext();
+
   // Talking state
   const [isTalking, setIsTalking] = useState(false);
+
   const [scale, setScale] = useState(0.1);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-  
+
   // Face state
   const { eyeScale, mouthScale } = useFace();
   const hoverPosition = useHover();
@@ -53,42 +49,6 @@ export default function BasicFace({
     speed: 0.075,
     isActive: isTalking,
   });
-
-  // Load images
-  useEffect(() => {
-    let loadedCount = 0;
-    const totalImages = (textureUrl ? 1 : 0) + (hatUrl ? 1 : 0);
-    
-    if (totalImages === 0) {
-      setImagesLoaded(true);
-      return;
-    }
-    
-    const checkAllLoaded = () => {
-      loadedCount++;
-      if (loadedCount === totalImages) {
-        setImagesLoaded(true);
-      }
-    };
-    
-    if (textureUrl) {
-      const img = new Image();
-      img.crossOrigin = 'anonymous';
-      img.onload = checkAllLoaded;
-      img.onerror = checkAllLoaded;
-      img.src = textureUrl;
-      textureImageRef.current = img;
-    }
-    
-    if (hatUrl) {
-      const img = new Image();
-      img.crossOrigin = 'anonymous';
-      img.onload = checkAllLoaded;
-      img.onerror = checkAllLoaded;
-      img.src = hatUrl;
-      hatImageRef.current = img;
-    }
-  }, [textureUrl, hatUrl]);
 
   useEffect(() => {
     function calculateScale() {
@@ -115,18 +75,9 @@ export default function BasicFace({
 
   // Render the face on the canvas
   useEffect(() => {
-    if (!imagesLoaded) return;
-    
     const ctx = canvasRef.current?.getContext('2d')!;
-    renderBasicFace({ 
-      ctx, 
-      mouthScale, 
-      eyeScale, 
-      color,
-      textureImage: textureImageRef.current || undefined,
-      hatImage: hatImageRef.current || undefined,
-    });
-  }, [canvasRef, volume, eyeScale, mouthScale, color, scale, imagesLoaded]);
+    renderBasicFace({ ctx, mouthScale, eyeScale, color });
+  }, [canvasRef, volume, eyeScale, mouthScale, color, scale]);
 
   return (
     <canvas

d98bfc446380c69140146f1a6043e98e32671607
aifakepro <aifake.pro@gmail.com>
2025-10-27 19:20:17 +0200

    Update basic-face-render.ts

--- components/demo/basic-face/basic-face-render.ts
+++ components/demo/basic-face/basic-face-render.ts
@@ -114,7 +114,7 @@ export function renderBasicFace(props: BasicFaceProps) {
     const isMobile = width < 780;
     
     // На мобильных делаем шапку меньше
-    const hatWidthRatio = isMobile ? 0.7 : 0.8;
+    const hatWidthRatio = isMobile ? 0.7 : 0.9;
     const hatWidth = width * hatWidthRatio;
     
     // Рассчитываем высоту с сохранением пропорций

84726e259ea5566c4728013bbab142464f6eee21
aifakepro <aifake.pro@gmail.com>
2025-10-28 18:39:39 +0200

    Update BasicFace.tsx

--- components/demo/basic-face/BasicFace.tsx
+++ components/demo/basic-face/BasicFace.tsx
@@ -109,7 +109,7 @@ export default function BasicFace({
           marginLeft: -canvasSize / 2,
           display: 'block',
           pointerEvents: 'none',
-          transform: `translateY(${hoverPosition+50}px) rotate(${tiltAngle}deg)`,
+          transform: `translateY(${hoverPosition+150}px) rotate(${tiltAngle}deg)`,
         }}
       />
     </>
